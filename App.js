ad91ae0349fc0e372072335771b3cd8dc1026f8f
Karthikeyan Subramani <karthi@Karthikeyans-MacBook-Air.local>
2025-04-25 10:40:18 +0530

    added browserslist and added npm react instead cdn link

--- App.js
+++ App.js
@@ -20,30 +20,37 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); */
 
 //Next level React element code for nested element creation - for children/siblings- with 2 child
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   {
-    id: "parent",
+    id: "parent"
   },
   [
     React.createElement(
       "div",
       {
         id: "child1",
+        key: "child1",
+        
       },
       [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
+        React.createElement("h1", {key:"child1h1"}, "I am an h1 tag"),
+        React.createElement("h2", {key:"child1h2"}, "I am an h2 tag"),
       ]
     ),
     React.createElement(
       "div",
       {
         id: "child2",
+        key: "child2",
       },
       [
-        React.createElement("h1", {}, "I am an h1 tag"),
-        React.createElement("h2", {}, "I am an h2 tag"),
+        React.createElement("h1", {key:"child2h1"}, "I am an h1 tag"),
+        React.createElement("h2", {key:"child2h2"}, "I am an h2 tag"),
       ]
     ),
   ]
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Karthik React Code
+#Karthik React Code
+
+
+#Parcel
+    -   DEv Build
+    -Different build for Dev and Prod
+    -Local Server
+    -HMR = Hot Module Replacement
+    -Web worker
+    -service worker
+    -Transpilation
+    -Treeshaking
+    -File watching Algorithm written in C++
+    -caching - Faster Builds(.parcel-caching)
+    Image optimization
+    -minification
+    -bundling
+    compressing
+    -consistent hashing
+    -Code splitting
+    Differential building  - supports older browser
+    -Diagnostic
+    -Error Handling
+    -HTTPS
+
